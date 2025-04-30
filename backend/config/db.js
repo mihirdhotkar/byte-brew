0f94c5af19492e6c90526f99c13969449a06aeb3
Mihir dhotkar <mihirdhotkar03@gmail.com>
2025-04-30 17:04:18 +0530

    fixing mongo error

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      process.env.MONGO_URL
+      process.env.MONGO_URL,
+      {
+        tlsAllowInvalidCertificates: true,
+      }
     )
     .then(() =>console.log("DB Connected"));
 };
